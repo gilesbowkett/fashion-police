69281928a503baa1d96909ff2148531491e08457
Giles Bowkett <gilesb@gmail.com>
2013-05-06 21:52:32 -0700

    beginning implementation

--- lib/fashion_police.rb
+++ lib/fashion_police.rb
@@ -2,14 +2,61 @@ class FashionPolice
   attr_reader :errors
   attr_accessor :rules
 
-  def initialize
-    @errors = []
-    @rules = [ lambda {|string| string != "\t"} ]
+  class SpacesNotTabs
+    def test(string)
+      not string.match(/\t/)
+    end
+
+    def error_message
+      "Use spaces, not tabs"
+    end
   end
 
-  def permit?(string)
+  class FourSpaces
+    def test(string)
+      spaces = string.match(/^(\s+)\S/)
+      return true unless spaces && spaces[1]
+
+      return (spaces[1].length % 4 == 0)
+    end
+
+    def error_message
+      "Four spaces per indentation level"
+    end
+  end
+
+  class NoSpacesInFunctionDeclarations
+    def test(string)
+      return true if string.match(/function\(\.*\)\{/)
+      return false if string.match(/function\s+\(\)\s+\{/)
+    end
+
+    def error_message
+      "No spaces in function declarations"
+    end
+  end
+
+  class BadCode < Exception; end
+
+  def initialize
+    @errors = []
+    @rules = [ SpacesNotTabs.new,
+               FourSpaces.new,
+               NoSpacesInFunctionDeclarations.new ]
+  end
+
+  def investigate(code)
+    code.split("\n").each_with_index do |line, index|
+      raise(BadCode) unless permit?(index, line)
+    end
+  end
+
+  def permit?(line_number, string)
     @rules.inject(true) do |memo, rule|
-      memo = false unless rule[string]
+      unless rule.test(string)
+        memo = false
+        @errors << {line_number => rule.error_message}
+      end
       memo
     end
   end
--- spec/fashion_police_spec.rb
+++ spec/fashion_police_spec.rb
@@ -1,25 +1,75 @@
 require_relative("../lib/fashion_police")
 
-describe "FashionPolice enforces a coding style which" do
-  before do
-    @fashion_police = FashionPolice.new
-  end
-
-  describe "on error" do
-    it "returns line number"
-    it "returns error message"
-  end
+describe "FashionPolice provides a tool which" do
 
   it "takes a file as input"
 
-  it "uses spaces, not tabs" do
-    @fashion_police.permit?("	").should be_false
-    @fashion_police.errors.should == [{1 => "Use spaces, not tabs"}]
+  it "detects violations of style rules" do
+    bad_code = <<-BAD_CODE
+  function (argument) {
+    console.log('wrong indentation style');
+  }
+    BAD_CODE
+
+    expect( lambda {FashionPolice.new.investigate(bad_code)} ).to raise_error(FashionPolice::BadCode)
   end
 
-  it "uses four spaces per indent"
-    # number_of_spaces % 4 == 0
-  it "puts no spaces in function declarations"
+  describe "on error" do
+
+    it "returns line number"
+
+    it "returns error message"
+
+  end
+
+end
+
+describe "FashionPolice enforces a coding style which" do
+
+  it "uses spaces, not tabs" do
+    FashionPolice::SpacesNotTabs.new.test("				").should be_false
+  end
+
+  describe "uses four spaces per indent" do
+
+    it "at zero levels in" do
+      FashionPolice::FourSpaces.new.test("function").should be_true
+    end
+
+    it "at 0.5 levels in" do
+      FashionPolice::FourSpaces.new.test("  function").should be_false
+    end
+
+    it "at one level in" do
+      FashionPolice::FourSpaces.new.test("    function").should be_true
+    end
+
+    it "at 1.5 levels in" do
+      FashionPolice::FourSpaces.new.test("      function").should be_false
+    end
+
+    it "at two levels in" do
+      FashionPolice::FourSpaces.new.test("        function").should be_true
+    end
+
+  end
+
+  describe "puts no spaces in function declarations" do
+    before do
+      @rule = FashionPolice::NoSpacesInFunctionDeclarations.new
+    end
+
+    it "positive case" do
+      @rule.test("function(){ return true; }").should be_true
+    end
+
+    it "negative case" do
+      @rule.test("function (argument) { return true; }").should be_false
+    end
+
+  end
+
   it "puts spaces around arguments in parentheses"
+
 end
 
