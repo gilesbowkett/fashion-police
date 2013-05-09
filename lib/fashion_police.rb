21c124071bbb368d074695cb79caac0d4cfcc3c9
Giles Bowkett <gilesb@gmail.com>
2013-05-08 17:59:32 -0700

    tweak in prep for integration specs

--- lib/fashion_police.rb
+++ lib/fashion_police.rb
@@ -81,7 +81,7 @@ class FashionPolice
   class SpacesAroundArgumentsInParens
     def test(string)
       return true if string.match(/\( .* \)/)
-      return true if string.match(/\S+\(function\(/)
+      return true if string.match(/\S*\(function\(/)
       return true if string.match(/\S+\(["'].*["']\)/)
       return false if string.match(/\(\S+\)/)
 
